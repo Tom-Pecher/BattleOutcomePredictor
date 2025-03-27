1f7e983cdfac5e2417a0dedc6eb9692483110e1a
main <tompecher.official@gmail.com>
2025-03-27 04:34:05 +0000

    Implemented plotting

--- main.py
+++ main.py
@@ -3,37 +3,47 @@ from unit import Unit
 from team import Team
 from battle import Battle
 
-# for d in range(700, 800):
-#     d = d/100
+from matplotlib import pyplot as plt
 
-#     attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
-#     defenders = Team(name="Defenders", units=[Unit(hp=135, damage=d) for _ in range(5)])
-#     b = Battle(attackers, defenders, print_console=False)
-#     outcomes = []
-#     for _ in range(10000):
-#         outcomes.append(b.simulate(length=15, reset_all=True))
+x = []
+y = []
+for d in range(0, 100+1):
+    d = d/10
 
-#     print(f"{d}: {sum(outcomes)/len(outcomes)}")
+    attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
+    defenders = Team(name="Defenders", units=[Unit(hp=135, damage=d) for _ in range(5)])
+    b = Battle(attackers, defenders, print_console=False)
+    outcomes = []
+    for _ in range(10000):
+        outcomes.append(b.simulate(length=100, reset_all=True))
 
-D1 = 7.5 - 1e-15
-
-attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
-defenders = Team(name="Defenders", units=[Unit(hp=135, damage=D1) for _ in range(5)])
-b = Battle(attackers, defenders, print_console=False)
-outcomes = []
-for _ in range(10000):
-    outcomes.append(b.simulate(length=15, reset_all=True))
-
-print(f"{D1}: {sum(outcomes)/len(outcomes)}")
+    x.append(d)
+    y.append(sum(outcomes)/len(outcomes))
+    print(f"{d}: {sum(outcomes)/len(outcomes)}")
 
 
-D2 = 7.5 - 1e-16
+plt.scatter(x, y)
+plt.show()
 
-attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
-defenders = Team(name="Defenders", units=[Unit(hp=135, damage=D2) for _ in range(5)])
-b = Battle(attackers, defenders, print_console=False)
-outcomes = []
-for _ in range(10000):
-    outcomes.append(b.simulate(length=15, reset_all=True))
+# D1 = 7.5 - 1e-15
 
-print(f"{D2}: {sum(outcomes)/len(outcomes)}")
+# attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
+# defenders = Team(name="Defenders", units=[Unit(hp=135, damage=D1) for _ in range(5)])
+# b = Battle(attackers, defenders, print_console=False)
+# outcomes = []
+# for _ in range(10000):
+#     outcomes.append(b.simulate(length=15, reset_all=True))
+
+# print(f"{D1}: {sum(outcomes)/len(outcomes)}")
+
+
+# D2 = 7.5 - 1e-16
+
+# attackers = Team(name="Attackers", units=[Unit(hp=60, damage=15) for _ in range(5)])
+# defenders = Team(name="Defenders", units=[Unit(hp=135, damage=D2) for _ in range(5)])
+# b = Battle(attackers, defenders, print_console=False)
+# outcomes = []
+# for _ in range(10000):
+#     outcomes.append(b.simulate(length=15, reset_all=True))
+
+# print(f"{D2}: {sum(outcomes)/len(outcomes)}")
